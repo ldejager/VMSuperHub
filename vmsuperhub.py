c58918dd4ad639fb7280a2bde2a9feb6a5fd0c5f
Leon de Jager <leondejager@gmail.com>
2014-12-13 15:34:28 +0000

    finished up processing part

--- vmsuperhub.py
+++ vmsuperhub.py
@@ -16,6 +16,10 @@ class SuperHub(object):
     Virgin Media SuperHub Statistics Class
     """
 
+    CARBON_SERVER = '0.0.0.0'
+    CARBON_PORT = 2003
+    INTERVAL = 60
+
     def __init__(self):
         """
         Initializing
@@ -24,10 +28,8 @@ class SuperHub(object):
     def __get_gateway__(self):
         """
         Get default gateway by reading /proc/net/route
-
-        :return: default_gateway
+        If running windows, adjust to your needs.
         """
-
 
         with open("/proc/net/route") as fh:
             for line in fh:
@@ -40,25 +42,35 @@ class SuperHub(object):
     def __get_upstream_stats__(self):
         """
         Get upstream statistics (Upstream Power)
-
-        :return: upstream_power_levels
         """
 
-        url = urllib2.urlopen("http://" + self.__get_gateway__() + "/cgi-bin/VmRouterStatusUpstreamCfgCgi")
-        parse = BeautifulSoup(url.read())
+        try:
+            url = urllib2.urlopen("http://" + self.__get_gateway__() + "/cgi-bin/VmRouterStatusUpstreamCfgCgi", timeout=10)
+            parse = BeautifulSoup(url.read())
 
-        power_label = parse.find(text="Power Level (dBmV)")
-        power_table = power_label.parent.parent
-        power = power_table.findAll('td')
-        power_levels = [power[1].text, power[2].text, power[3].text, power[4].text]
+            power_label = parse.find(text="Power Level (dBmV)")
+            power_table = power_label.parent.parent
+            power = power_table.findAll('td')
+            power_levels = [power[1].text, power[2].text, power[3].text, power[4].text]
 
-        return power_levels
+            power_data = []
+
+            ts = int(time.time())
+
+            for (i, value) in enumerate(power_levels, start=1):
+                power_format = "virgin.modem.stats.us{0}_power {1} {2}".format(i, value, ts)
+                power_data.append(power_format)
+
+            return power_data
+
+        except urllib2.URLError, e:
+
+            print "There was an error: %r" % e
+
 
     def __get_downstream_stats__(self):
         """
         Get downstream statistics (Downstream Power)
-
-        :return: downstream_power_levels
         """
 
         url = urllib2.urlopen("http://" + self.__get_gateway__() + "/cgi-bin/VmRouterStatusDownstreamCfgCgi")
@@ -69,13 +81,19 @@ class SuperHub(object):
         power = power_table.findAll('td')
         power_levels = [power[1].text, power[2].text, power[3].text, power[4].text, power[5].text, power[6].text, power[7].text, power[8].text]
 
-        return power_levels
+        power_data = []
+
+        ts = int(time.time())
+
+        for (i, value) in enumerate(power_levels, start=1):
+            power_format = "virgin.modem.stats.ds{0}_power {1} {2}".format(i, value, ts)
+            power_data.append(power_format)
+
+        return power_data
 
     def __get_snr__(self):
         """
         Get modem SNR
-
-        :return: SNR_levels
         """
 
         url = urllib2.urlopen("http://" + self.__get_gateway__() + "/cgi-bin/VmRouterStatusDownstreamCfgCgi")
@@ -86,19 +104,35 @@ class SuperHub(object):
         snr = snr_table.findAll('td')
         snr_levels = [snr[1].text, snr[2].text, snr[3].text, snr[4].text, snr[5].text, snr[6].text, snr[7].text, snr[8].text]
 
-        return snr_levels
+        snr_data = []
+
+        ts = int(time.time())
+
+        for (i, value) in enumerate(snr_levels, start=1):
+            snr_format = "virgin.modem.stats.ds{0}_rx {1} {2}".format(i, value, ts)
+            snr_data.append(snr_format)
+
+        return snr_data
+
+
+    def __carbon_send__(self, data_stream):
+        """
+        Send data to Carbon
+        """
+
+        print 'sending message:\n%s' % data_stream
+        sock = socket.socket()
+        sock.connect((SuperHub.CARBON_SERVER, SuperHub.CARBON_PORT))
+        sock.sendall(data_stream)
+        sock.close()
 
 
 if __name__ == '__main__':
 
-    # Need to finish this
-
     SuperHub = SuperHub()
 
-    print SuperHub.__get_upstream_stats__()
-    print SuperHub.__get_downstream_stats__()
-    print SuperHub.__get_snr__()
-
-
-
-
+    while True:
+        data = SuperHub.__get_upstream_stats__() + SuperHub.__get_downstream_stats__() + SuperHub.__get_snr__()
+        data_stream = '\n'.join(data) + '\n'
+        SuperHub.__carbon_send__(data_stream)
+        time.sleep(SuperHub.INTERVAL)
